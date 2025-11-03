dec934fe814ede4ee7ee79bddfe283dec006195f
@Tanxnllz <tristanvngs21@gmail.com>
2025-11-03 09:28:03 +0800

    Reconstruction with suggestion of domirez and babasa

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+// Shared Firebase config
+export const firebaseConfig = {
+  apiKey: "AIzaSyBWEsXe-FUspLVrQbWx9W9fdJjhEZxzs-c",
+  authDomain: "printit-web.firebaseapp.com",
+  databaseURL: "https://printit-web-default-rtdb.firebaseio.com",
+  projectId: "printit-web",
+  storageBucket: "printit-web.firebasestorage.app",
+  messagingSenderId: "432024876127",
+  appId: "1:432024876127:web:03f56715a495f801cd97ea",
+  measurementId: "G-MLX52XE058"
+};
